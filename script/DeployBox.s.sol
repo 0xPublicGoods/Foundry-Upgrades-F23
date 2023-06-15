ace7ec7363b4f34f4a1706ed52d8735a1806659c
0xPublicGoods <0xpublicgoods@campsbay.com>
2023-06-15 15:43:14 +0200

    wip

--- script/DeployBox.s.sol
+++ script/DeployBox.s.sol
@@ -12,11 +12,14 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 contract DeployBox is Script {
     function run() external returns (address) {
         address proxy = deployBox();
+        return proxy;
     }
 
     function deployBox() public returns (address) {
+        vm.startBroadcast();
         BoxV1 box = new BoxV1(); // implementation (Logic)
         ERC1967Proxy proxy = new ERC1967Proxy(address(box), ""); // proxy
+        vm.stopBroadcast();
         return address(proxy);
     }
 }

bf6ddcbfdeb5f30fdcdf36a602bbf0c1f6c8c78b
0xPublicGoods <0xpublicgoods@campsbay.com>
2023-06-15 15:28:42 +0200

    wip

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /**
  * @title BoxV1 Contract
@@ -10,8 +12,18 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
  * @notice BoxV1 Contract UUPS Proxy
  * @dev BoxV1 Contract
  */
-contract BoxV1 is UUPSUpgradeable {
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); // sets msg.sender as owner
+    }
 
     function getNumber() external view returns (uint256) {
         return number;

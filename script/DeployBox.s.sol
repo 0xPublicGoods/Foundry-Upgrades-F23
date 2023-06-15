91a42f7d14a523b018110ef6a0642f0f908e13c2
0xPublicGoods <0xpublicgoods@campsbay.com>
2023-06-15 15:38:07 +0200

    wip

---
+++ script/DeployBox.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * Imports
+ */
+import {Script} from "forge-std/Script.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract DeployBox is Script {
+    function run() external returns (address) {
+        address proxy = deployBox();
+    }
+
+    function deployBox() public returns (address) {
+        BoxV1 box = new BoxV1(); // implementation (Logic)
+        ERC1967Proxy proxy = new ERC1967Proxy(address(box), ""); // proxy
+        return address(proxy);
+    }
+}
